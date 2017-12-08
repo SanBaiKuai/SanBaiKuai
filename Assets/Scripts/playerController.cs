b534ad24f77a4b408b03d388c238d18f9570a042
Kyle Chu <ktnc.985@gmail.com>
2017-12-08 14:58:45 +0800

    add shrink ability

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -14,16 +14,17 @@ public class playerController : MonoBehaviour {
 	private Vector3 direction;
 	private bool onGround = true;
 	private Abilities currAbility;
+	private bool isShrunk = false;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = this.GetComponent<Rigidbody2D> ();
-		direction = this.transform.localScale;
-		currAbility = Abilities.superJump;
+		currAbility = Abilities.shrink;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		direction = this.transform.localScale;
 		Vector2 velo = rb2d.velocity;
 		//left-right movements
 		if (Input.GetKey(KeyCode.W))
@@ -69,6 +70,13 @@ public class playerController : MonoBehaviour {
 				velo.y = superJump * Time.deltaTime;
 				rb2d.velocity = velo;
 			}
+		} else if (currAbility == Abilities.shrink) {
+			if (!isShrunk) {
+				this.gameObject.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+			} else {
+				this.gameObject.transform.localScale = new Vector3 (1f, 1f, 1f);
+			}
+			isShrunk = !isShrunk;
 		}
 	}
 
