d010613b3ffd486f2330f8b41a5365449df34886
khooroko <khoowp25@hotmail.com>
2017-12-08 14:40:44 +0800

    Add weakwall

---
+++ Assets/Scripts/WeakWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakWall : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Wallbreaker") {
+            StartCoroutine(Break());
+        }
+    }
+
+    IEnumerator Break() {
+        anim.SetTrigger("Break");
+        yield return new WaitForSeconds(2f);
+        Destroy(this);
+    }
+}
