1656722fc6bfc13888d1b038ab17692224b1ba6e
khooroko <khoowp25@hotmail.com>
2017-12-08 15:29:41 +0800

    Add background and gamemanager prefab

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject musicManager;
+    public GameObject player;
+
+    private AudioSource[] music;
+
+	// Use this for initialization
+	void Start () {
+        music = musicManager.GetComponentsInChildren<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (player.isDead) {
+            music[0].Stop();
+            music[1].Play();
+        // reload current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //}
+	}
+}
