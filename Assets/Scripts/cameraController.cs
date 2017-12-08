2690f1e20d39e9cf9aab9892ec56a18e5b77a50f
Kyle Chu <ktnc.985@gmail.com>
2017-12-08 14:34:01 +0800

    add super jump for player and camera control script

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+	
+	public float xMin;
+	public float xMax;
+	public float yMin;
+	public float yMax;
+
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null) {
+			transform.position = new Vector3 (Mathf.Clamp (player.position.x, xMin, xMax), 
+				Mathf.Clamp (player.position.y, yMin, yMax), transform.position.z);
+		}
+	}
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,16 +6,20 @@ public class playerController : MonoBehaviour {
 
 	public float speed = 100;
 	public float jump = 250;
+	public float superJump = 8000;
+
+	private enum Abilities {superJump, shrink, wallBreak, ghostWalk};
 
 	private Rigidbody2D rb2d;
 	private Vector3 direction;
 	private bool onGround = true;
+	private Abilities currAbility;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = this.GetComponent<Rigidbody2D> ();
 		direction = this.transform.localScale;
-		
+		currAbility = Abilities.superJump;
 	}
 	
 	// Update is called once per frame
@@ -52,7 +56,21 @@ public class playerController : MonoBehaviour {
 			//anim.SetBool("Walking", false);
 		}
 			
-		
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			executeAbility ();
+		}
 		
 	}
+
+	private void executeAbility() {
+		Vector2 velo = rb2d.velocity;
+		if (currAbility == Abilities.superJump) {
+			if (onGround) {
+				velo.y = superJump * Time.deltaTime;
+				rb2d.velocity = velo;
+			}
+		}
+	}
+
 }
+
